e5c1ca380b76b7e2a894d59050597f16ac35a8ab
Akihiko-source <hyra.31.u@gmail.com>
2025-06-08 12:59:06 -0500

    Delete src directory

--- src/main/java/com/proyecto/integrador1/proyecto_integrador/repositories/DispositivoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.integrador1.proyecto_integrador.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.integrador1.proyecto_integrador.entities.Dispositivo;
-
-public interface DispositivoRepository extends JpaRepository<Dispositivo,Long> {
-
-}
